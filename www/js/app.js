50f24d533db99feeac852863ff39666fb6845c78
Toatzin Padilla <A01225341@itesm.mx>
2016-03-18 12:39:43 -0600

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,17 @@
-// Ionic Starter App
 
+/*This file is part of Asphalt
+  Asphalt is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ Asphalt is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Asphalt.  If not, see <http://www.gnu.org/licenses/>.
+    */
+// Ionic Starter App    
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
